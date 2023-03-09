cf0b95384efc4441f62fa7543a26aa8829646a0b
Americano.Dev <andrenations@gmail.com>
2023-03-09 06:00:06 -0500

    redeployed

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/andresilva', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
