88cb756c67e46c6964657aeaa32f6a64ec4a6313
Americano.Dev <andrenations@gmail.com>
2024-08-28 16:20:33 -0400

    change conneticon string

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI ||'mongodb+srv://bookSearch:booksearch@cluster0.wqut3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/andresilva', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
